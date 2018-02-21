f3c7b53d6bda632c93f09be0a4f1bcfe3bf05e32
Hillary Sousa <hillaryhpf@gmail.com>
2018-02-21 17:25:28 -0300

    Busca funcionando e retornando local e país através de input e select

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const API_KEY = '6dc1c1251101d45f3c7d69421871866e';
-const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},br`;
+export function fetchWeather(city, country) {
+  const url = `${ROOT_URL}&q=${city},${country}`;
   const request = axios.get(url);
   return {
     type: FETCH_WEATHER,
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,26 +1,39 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {CountryDropdown} from 'react-country-region-selector';
+import {fetchWeather} from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
-    this.state = {term: ''};
+    this.state = {term: '', country: ''};
     this.onInputChange = this.onInputChange.bind(this);
+    this.onCountryChange = this.onCountryChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
     this.setState({term: event.target.value});
   }
 
+  onCountryChange(val) {
+    console.log(this.state.country);
+    this.setState({country: val});
+  }
+
   onFormSubmit(event) {
     event.preventDefault();
-
     // Buscar os dados da previsão do tempo
+    this.props.fetchWeather(this.state.term, this.state.country);
+    this.setState({term: ''}, {country: ''});
   }
 
   render() {
     return (
       <form onSubmit={this.onFormSubmit} className="input-group">
         <input placeholder="Get a five-day forecast in your favorite cities" className="form-control" value={this.state.term} onChange={this.onInputChange} />
+        <CountryDropdown value={this.state.country} onChange={(val) => this.onCountryChange(val)} classes="form-control" valueType="short" />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
@@ -28,3 +41,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
